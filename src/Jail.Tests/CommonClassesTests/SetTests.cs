3692e04d7696b35406f576bfd4cc4442a230eff7
hoborg91 <hoborg91@gmail.com>
2020-02-03 09:28:05 +0300

    Improve Set tests

--- src/Jail.Tests/CommonClassesTests/SetTests.cs
+++ src/Jail.Tests/CommonClassesTests/SetTests.cs
@@ -11,26 +11,52 @@ namespace Jail.Tests.CommonClassesTests {
         [Test]
         public void CommonTests() {
             // Arrange, Act, Assert
-            var set = new Set<int>();
-            var setAsCollection = (ICollection<int>)set;
-            setAsCollection.Add(1);
-            setAsCollection.Add(2);
-            Assert.AreEqual(2, setAsCollection.Count);
-            CollectionAssert.AreEquivalent(new[] { 1, 2 }, setAsCollection);
+            var sut = new Set<int>();
+            var asCollection = (ICollection<int>)sut;
+            asCollection.Add(1);
+            asCollection.Add(2);
+            Assert.AreEqual(2, asCollection.Count);
+            CollectionAssert.AreEquivalent(new[] { 1, 2 }, asCollection);
 
-            setAsCollection.Add(2);
-            Assert.AreEqual(2, setAsCollection.Count);
-            CollectionAssert.AreEquivalent(new[] { 1, 2 }, setAsCollection);
+            asCollection.Add(2);
+            Assert.AreEqual(2, asCollection.Count);
+            CollectionAssert.AreEquivalent(new[] { 1, 2 }, asCollection);
 
-            setAsCollection.Remove(2);
-            Assert.AreEqual(1, setAsCollection.Count);
-            CollectionAssert.AreEquivalent(new[] { 1 }, setAsCollection);
+            asCollection.Remove(2);
+            Assert.AreEqual(1, asCollection.Count);
+            CollectionAssert.AreEquivalent(new[] { 1 }, asCollection);
+        }
+
+        [Test]
+        public void CustomEqualityComparer() {
+            // Arrange
+            var sut = new Set<int>(new AlwaysNotEqual());
+
+            // Act
+            sut.Add(1);
+            sut.Add(1);
+
+            // Assert
+            Assert.AreEqual(2, sut.Count);
+        }
+
+        [Test]
+        public void CustomEqualityComparer_WithCollection() {
+            // Arrange
+            var collection = new[] { 1, 1, };
+
+            // Act
+            var sut = new Set<int>(collection, new AlwaysNotEqual());
+
+            // Assert
+            Assert.AreEqual(2, sut.Count);
         }
 
         [Test]
         public void Proxy() {
             // Arrange
             var mock = new Mock<ISet<int>>();
+            mock.Setup(s => s.Equals(It.IsAny<object>()));
             var sut = new Set<int>(mock.Object);
 
             var argContains = -100;
@@ -52,8 +78,10 @@ namespace Jail.Tests.CommonClassesTests {
             sut.IsSupersetOf(iEnumerable);
             sut.Overlaps(iEnumerable);
             sut.Remove(argRemove);
+            sut.SetEquals(iEnumerable);
             sut.SymmetricExceptWith(iEnumerable);
             sut.UnionWith(iEnumerable);
+            sut.Equals(iEnumerable);
 
             // Assert
             mock.Verify(a => a.Clear(), Times.Once);
@@ -68,8 +96,49 @@ namespace Jail.Tests.CommonClassesTests {
             mock.Verify(a => a.IsSupersetOf(iEnumerable), Times.Once);
             mock.Verify(a => a.Overlaps(iEnumerable), Times.Once);
             mock.Verify(a => a.Remove(argRemove), Times.Once);
+            mock.Verify(a => a.SetEquals(iEnumerable), Times.Once);
             mock.Verify(a => a.SymmetricExceptWith(iEnumerable), Times.Once);
             mock.Verify(a => a.UnionWith(iEnumerable), Times.Once);
+            mock.Verify(a => a.Equals(iEnumerable));
+        }
+
+        [Test]
+        public void Test_ToString() {
+            // Arrange
+            var sut = new Set<int>();
+
+            // Act
+            var result = sut.ToString();
+
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Test]
+        public void Test_GetHashCode() {
+            // Arrange
+            var underlyingSet = new HashSet<int>();
+            var sut = new Set<int>(underlyingSet);
+            var expected = underlyingSet.GetHashCode();
+
+            // Act
+            var result = sut.GetHashCode();
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        private sealed class AlwaysNotEqual : IEqualityComparer<int>
+        {
+            public bool Equals(int x, int y)
+            {
+                return false;
+            }
+
+            public int GetHashCode(int obj)
+            {
+                return 0;
+            }
         }
     }
 }
